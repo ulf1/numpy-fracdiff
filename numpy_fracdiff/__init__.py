85b7650576b790a4cc6162146eef0c12ecbaa098
UH <554c46@gmail.com>
2021-04-08 12:34:09 +0200

    pkg version variable added

--- numpy_fracdiff/__init__.py
+++ numpy_fracdiff/__init__.py
@@ -1,1 +1,3 @@
+__version__ = '0.3.2'
+
 from .fracdiff_fn import fracdiff
--- setup.py
+++ setup.py
@@ -2,8 +2,18 @@ from setuptools import setup
 import pypandoc
 
 
+def get_version(path):
+    with open(path, "r") as fp:
+        lines = fp.read()
+    for line in lines.split("\n"):
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(name='numpy-fracdiff',
-      version='0.3.1',
+      version=get_version("numpy_fracdiff/__init__.py"),
       description='Fractional Difference for Time Series',
       long_description=pypandoc.convert('README.md', 'rst'),
       url='http://github.com/ulf1/numpy-fracdiff',
